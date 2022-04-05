ca013ece216aba37dd5825a368f0768a508ee72a
JulesZYTB <93528905+JulesZYTB@users.noreply.github.com>
2022-04-05 08:40:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
 let http = require('http');
-let fs = require('fs');
+let router = require('./routes');
 
 let handleRequest = (request, response) => {
     response.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    fs.readFile('*/index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
+
 };
 
-http.createServer(handleRequest).listen(3000); 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+http.createServer(router.handleRequest).listen(8000); 
  
